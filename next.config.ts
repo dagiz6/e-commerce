eed511e67b44c09c936f30249dcc59c5a56af57a
unknown <dagimt369@gmail.com>
2025-07-08 11:53:01 +0200

    Api

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
+  optimizeFonts: false,
   images: {
     unoptimized: true,
     domains: ["images.pexels.com", "lh3.googleusercontent.com"],
